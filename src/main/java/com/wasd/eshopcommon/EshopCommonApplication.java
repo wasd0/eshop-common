3c96139e1699c334eff1ab6dde1c0acd41775918
wasd <kirshvedov13@gmail.com>
2024-05-19 20:23:37 +0200

    init project

---
+++ src/main/java/com/wasd/eshopcommon/EshopCommonApplication.java
@@ -0,0 +1,12 @@
+package com.wasd.eshopcommon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EshopCommonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EshopCommonApplication.class, args);
+	}
+}
